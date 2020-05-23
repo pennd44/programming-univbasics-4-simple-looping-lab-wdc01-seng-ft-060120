623bbf83a1446ae938691c8e065ba8c64d4376cf
Penn M Diba <pdiba@oxbowschool.org>
2020-05-23 00:19:56 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -4,22 +4,30 @@ def loop_message_five_times(message)
   while count < 5 do
   puts message
   coint += 1
+  end
 end
 
 def loop_message_n_times(message, limit)
   count = 0
   while count < limit do
-  puts message
-  count += 1
+    puts message
+    count += 1
+  end
 end
 
 def output_array(array)
   counter = 0 
   while counter<array.length do
-  puts array[counter]
-  counter += 1 
+    puts array[counter]
+    counter += 1 
+  end
 end
 
 def return_string_array(array)
+  count = 0 
+  while count < array.length do
+    array[count].to_s
+    count += 1
+  end
 end
 
